d6ddaca4c628067c1bbfa62496f0e3a20897aec8
Александр Танчинец <123393302+alextanchi@users.noreply.github.com>
2024-02-13 22:10:21 +0500

    Update main.go

--- 13_Roman-to-Integer/main.go
+++ 13_Roman-to-Integer/main.go
@@ -1,5 +1,35 @@
 package main
 //нужно написать функцию преобразования 
 func main() {
-
+romanToInt(X)
 }
+
+func romanToInt(s string) int {
+    romanValues := map[byte]int{
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    result := 0
+    prevValue := 0
+
+    for i := len(s) - 1; i >= 0; i-- {
+        value := romanValues[s[i]]
+
+        if value < prevValue {
+            result -= value
+        } else {
+            result += value
+        }
+
+        prevValue = value
+    }
+
+    return result
+}
+
